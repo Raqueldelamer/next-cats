4861f11572ad5e98f33320caff39f767458ba680
Raquel Marie <raqueldelamer@gmail.com>
2024-10-19 14:19:13 -0500

    centering image works best with <center></center> lol

--- src/pages/cat/CatImage.js
+++ src/pages/cat/CatImage.js
@@ -1,10 +1,12 @@
 export default function CatImage(props) {
-    const imgURL = props.catData.image_link;
+    const image = props.catData.image_link;
     return ( 
         <div className= "m-4 p-4 flex-col items-center justify-center" >
-            <div class="card">
-            <img src={imgURL} alt="cat" />
+            <div class="card justify-center">
+            <center><img src={image} alt="cat" /></center> 
             </div>
         </div>
     );
 }
+
+// <center>LOLOL</center>
