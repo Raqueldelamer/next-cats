02e282f26dcb058d07e82878c19d13ef7d8b6b47
Raquel Marie <raqueldelamer@gmail.com>
2024-10-25 12:42:38 -0500

    update index.js in 2nd branch

--- src/pages/cat/CatImage.js
+++ src/pages/cat/CatImage.js
@@ -3,7 +3,7 @@ export default function CatImage(props) {
     return ( 
         <div className= "m-4 p-4 flex-col items-center justify-center" >
             <div class="card justify-center">
-            <center><img src={image} alt="cat" /></center> 
+            <center><img src={image} alt={cat.name} /></center> 
             </div>
         </div>
     );
--- src/pages/cat/index.js
+++ src/pages/cat/index.js
@@ -1,26 +1,27 @@
+import CatHeader from "./CatHeader";
 import { useEffect, useState } from "react";
+
 
 // DEMO https://github.com/rmccrear/cats-cats-cats-demo/blob/fetch-demo/src/pages/cat.js
 
 const catData = {
-    "length": "Medium",
-    "origin": "Saudi Arabia",
-    "image_link": "https://api-ninjas.com/images/cats/arabian_mau.jpg",
-    "family_friendly": 4,
-    "shedding": 2,
-    "general_health": 4,
+    "length": "17 to 21 inches, not including tail",
+    "origin": "Riverside, California, USA",
+    "image_link": "https://api-ninjas.com/images/cats/ragdoll_cats.jpg",
+    "family_friendly": 5,
+    "shedding": 4,
+    "general_health": 3,
     "playfulness": 4,
-    "meowing": 3,
-    "children_friendly": 4,
-    "stranger_friendly": 4,
-    "grooming": 5,
+    "meowing": 4,
+    "children_friendly": 5,
+    "grooming": 1,
     "intelligence": 4,
-    "other_pets_friendly": 3,
-    "min_weight": 8,
-    "max_weight": 16,
+    "other_pets_friendly": 4,
+    "min_weight": 10,
+    "max_weight": 20,
     "min_life_expectancy": 12,
-    "max_life_expectancy": 14,
-    "name": "Arabian Mau"
+    "max_life_expectancy": 17,
+    "name": "Ragdoll Cats"
     
     }
     console.log(catData);
@@ -30,10 +31,11 @@ export default function CatPage() {
     const [ cat, setCat ] = useState({});
 
     async function fetchCat() {
-        const result = await fetch("https://cats-cats-cats-demo.deno.dev/cats/mau");
+        const result = await fetch("https://api-ninjas.com/images/cats/ragdoll_cats.jpg");
         
         const data = await result.json();
         console.log(data);
+        console.log(catData);
 
         setCat(data);
         }
@@ -43,22 +45,36 @@ export default function CatPage() {
             fetchCat();
         }, []);
 
-
         return (
+        
+    
             <div>
                 <div>
-                    <h1 className="text-4xl">{cat.name}</h1>
+                    <h1 className="text-6xl font-mono font-bold text-center"><CatHeader /></h1><br />
+                    <h1 className="text-6xl font-mono font-bold text-center mt-9">The {cat.name}</h1>
                 </div>
-                <div>
+                <div className= "p-4 flex-col">
+                    <center><div className="card size-96 flex flex-wrap justify-center items-center ">
                     <img src={cat.image_link} alt={cat.name} />
-                </div>
+                    </div> </center>
                 <div>
-                    {`The ${cat.name} comes from ${cat.origin} `}
+                    <h1 className="text-3xl font-bold text-center">::Breed Description::</h1> 
+                    <h2 className="text-center text-1xl font-bold"> 
+                    The {cat.name} is native to {cat.origin} <br />
+                    The {cat.name} intelligence score is: {cat.intelligence} out of 5. <br /> 
+                    It's max life expectancy is {cat.max_life_expectancy} years.<br /> 
+                    Family Friendly Score: {cat.family_friendly} out of 5<br />
+                    Playfulness: {cat.playfulness} out of 5<br />
+                    Max Adult Weight: {cat.max_weight} lbs <br />
+                    </h2>
                 </div>
             </div>
+            </div>
+        
         );
 
 }
+
 
 
 
