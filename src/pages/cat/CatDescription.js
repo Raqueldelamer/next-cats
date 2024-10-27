0eabbef5650840cb6360999b24def95ba3f63b7b
Raquel Marie <raqueldelamer@gmail.com>
2024-10-27 08:12:20 -0500

    update CatDescription component

--- src/pages/cat/CatDescription.js
+++ src/pages/cat/CatDescription.js
@@ -6,9 +6,9 @@ export default function CatDescription (props) {
         <div>
             <h1 className="text-3xl text-black font-bold text-center">::Breed Description::</h1> 
             <p className= "text-1xl text-black font-bold text-center">The {props.name} is native to {props.origin} <br />
-            Family Friendly: {props.family_friendly} out of 5<br />
-            Playfulness: {props.playfulness} out of 5<br />
-            Intelligence: {props.intelligence} out of 5<br />
+            Family Friendly score: {props.family_friendly} out of 5<br />
+            Playfulness score: {props.playfulness} out of 5<br />
+            Intelligence score: {props.intelligence} out of 5<br />
             Ideal Adult Weight: {props.max_weight} lbs <br />
             Max Life Expectancy: {props.max_life_expectancy} years <br />
             </p>
