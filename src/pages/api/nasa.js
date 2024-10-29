17f7e9b42a8854461db781a09ab1161b010fd40a
Raquel Marie <raqueldelamer@gmail.com>
2024-10-29 00:36:18 -0500

    add nasa.js with api_key to fetch data

---
+++ src/pages/api/nasa.js
@@ -0,0 +1,21 @@
+
+
+async function getNasaData () {
+    const apiUrl ="https://api.nasa.gov/planetary/apod?api_key=qdKmIGSnIsK5iNoMCvpbQwkaWZuXIclEzQNe1E35" //api_key entered
+    const result = await fetch(apiUrl);
+    const data = await result.json()
+    return data;
+
+}
+
+export default async function handler(req, res) {
+    const nasa = await getNasaData();
+    console.log(nasa);
+    res.status(200).json({
+        Date: nasa.date,
+        Title: nasa.title,
+        hdURL: nasa.hdurl,
+        Description: nasa.explanation,
+    
+    });
+}
