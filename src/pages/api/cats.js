ffd03c0325723579d4dae7b0f930b41c7979b520
Raquel Marie <raqueldelamer@gmail.com>
2024-10-28 22:56:30 -0500

    add api/cats.js fetch data

---
+++ src/pages/api/cats.js
@@ -0,0 +1,20 @@
+async function getCatData () {
+    const url ="https://cats-cats-cats-demo.deno.dev/cats/abyssinian";
+    const result = await fetch(url);
+    const data = await result.json();
+    return data; 
+}
+export default async function handler(req, res) {
+    const cat = await getCatData();
+    console.log(cat);
+    res.status(200).json({
+        Name: cat.name,
+        Origin: cat.origin,
+        Image: cat.image_link,
+        Family_Friendly: cat.family_friendly,
+        Intelligence: cat.intelligence,
+        Playfulness: cat.playfulness,
+        Max_Weight: cat.max_weight,
+        Max_Life_Expectancy: cat.max_life_expectancy,
+    });
+}
