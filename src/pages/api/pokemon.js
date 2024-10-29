b837cceb529776fc070b68d687cbf2186f8d05c9
Raquel Marie <raqueldelamer@gmail.com>
2024-10-28 21:24:27 -0500

    api/pokemon.js fetch

---
+++ src/pages/api/pokemon.js
@@ -0,0 +1,21 @@
+
+async function getPokemon () {
+    const url ="https://pokeapi.co/api/v2/pokemon/meowth";
+    const result = await fetch(url);
+    const data = await result.json();
+    return data; 
+}
+export default async function handler(req, res) {
+    const pokemon = await getPokemon();
+    console.log(pokemon);
+    res.status(200).json({
+        pokemonName: pokemon.name,
+        pokemonCry: pokemon.cries.latest,
+        pokemonImgSrc: pokemon.sprites.front_default,
+        firstAbility: pokemon.abilities[0].ability.name,
+        secondAbility: pokemon.abilities[1].ability.name,
+        height: pokemon.height
+    });
+}
+
+
